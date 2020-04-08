5c908af43d22807863b6411f2e86587dd71ff1ce
刘家辉 <liujiahiu@qq.com>
2020-04-08 20:34:50 +0800

    2020-4-8

--- Temperature_control_box/__vm/.Temperature_control_box.vsarduino.h
+++ Temperature_control_box/__vm/.Temperature_control_box.vsarduino.h
@@ -6,54 +6,86 @@
 			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Generic STM32F103R series, Platform=STM32F1, Package=Arduino_STM32-master
+	Hardware: Arduino/Genuino Uno, Platform=avr, Package=arduino
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define DEBUG_LEVEL DEBUG_NONE
-#define BOARD_generic_stm32f103r
-#define VECT_TAB_ADDR 0x8000000
-#define ERROR_LED_PORT GPIOB
-#define ERROR_LED_PIN 1
-#define F_CPU 72000000L
-#define ARDUINO 10809
-#define ARDUINO_GENERIC_STM32F103R
-#define ARDUINO_ARCH_STM32F1
-#define CONFIG_MAPLE_MINI_NO_DISABLE_DEBUG 1
-#define SERIAL_USB
-#define GENERIC_BOOTLOADER
-#define MCU_STM32F103RC
-#define __STM32F1__
-#define ARDUINO_ARCH_STM32
+#define __AVR_atmega328p__
+#define __AVR_ATmega328P__
+#define __AVR_ATmega328p__
+#define F_CPU 16000000L
+#define ARDUINO 108010
+#define ARDUINO_AVR_UNO
+#define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
-
+#define _Pragma(x)
+#define __AVR__
 #define __inline__
-#define __asm__(x)
+#define __asm__(...)
 #define __extension__
-#define __ATTR_PURE__
-#define __ATTR_CONST__
 #define __inline__
 #define __volatile__
-
-#define __GNUC__ 6
-#define  __GNUC_MINOR__ 3
-#define  __GNUC_PATCHLEVEL__ 0
-#define _Static_assert(x)
-
-#undef __cplusplus
 #define __cplusplus 201103L
-typedef bool _Bool;
-#define __ARMCC_VERSION 400678
-#define __attribute__(noinline)
+
+//#define GCC_VERSION 40902
+//https://www.visualmicro.com/forums/YaBB.pl?num=1569762585/5#5
+#define __GNUC__             5
+#define __GNUC_MINOR__       4
+#define __GNUC_PATCHLEVEL__  0
+#define GCC_VERSION ((__GNUC__*10000)+(__GNUC_MINOR__*100)+__GNUC_PATCHLEVEL__)) 
+
+
+#define volatile(va_arg) 
+#define _CONST
+#define __builtin_va_start
+#define __builtin_va_end
+#define __attribute__(...)
+#define NOINLINE __attribute__((noinline))
+#define prog_void
+#define PGM_VOID_P int
+
+
+#ifndef __builtin_constant_p
+	#define __builtin_constant_p __attribute__((__const__))
+#endif
+#ifndef __builtin_strlen
+	#define __builtin_strlen  __attribute__((__const__))
+#endif
+
+
+#define NEW_H
+typedef void *__builtin_va_list;
+//extern "C" void __cxa_pure_virtual() {;}
+
+typedef int div_t;
+typedef int ldiv_t;
+
+
+typedef void *__builtin_va_list;
+//extern "C" void __cxa_pure_virtual() {;}
+
+
+
 #include "arduino.h"
 #include <pins_arduino.h> 
-#include <variant.h> 
-#include <board.cpp> 
-#undef cli
-#define cli()
+//#undef F
+//#define F(string_literal) ((const PROGMEM char *)(string_literal))
+#undef PSTR
+#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
+
+//typedef unsigned char uint8_t;
+//typedef unsigned int uint8_t;
+
+#define pgm_read_byte_near(address_short) uint8_t()
+#define pgm_read_byte(address_short) uint8_t() 
+#define pgm_read_word(address_short) uint16_t() 
+#define pgm_read_dword(address_short) uint32_t()
+#define pgm_read_float(address_short) float()
+#define pgm_read_ptr(address_short)   short()
+
 #include "Temperature_control_box.ino"
 #endif
 #endif
